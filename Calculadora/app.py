2979ffb7669275edba19942707452f6ad619856f
Lucas Souza <lucas.s.work@outlook.com>
2023-08-23 18:15:02 -0300

    Melhorando os comentários na calculadora

--- Calculadora/app.py
+++ Calculadora/app.py
@@ -16,33 +16,32 @@ entrada_b = input('Digite o segundo número: ')
 
 # verificando validade dos valores digitados
 
-if(entrada_a == '' or entrada_b == ''):
+if(entrada_a == '' or entrada_b == ''): # Se a ou b for vazio, siga por aqui
     if(entrada_a == ''):
         print('ERROR: Não foi digitado nenhum valor na primeira entrada, por favor digite.')
     else:
-        print('ERRO: Não foi digitado nenhum valor na segunda entrada, por favor digite')
-else:
-    if(operacao != 'a' and operacao != 'm' and operacao != 'd' and operacao != 's'):
+        print('ERRO: Não foi digitado nenhum valor na segunda entrada, por favor digite.')
+else: # Se a ou b não for vazio, siga por aqui
+    if(operacao != 'a' and operacao != 'm' and operacao != 'd' and operacao != 's'): 
         print('ERROR: Não foi digitada uma operação válida, por favor digite.')
-    else:
+    else: # Fim da maioria da maioria das validações
         valor_a = float(entrada_a.replace(',','.'))
         valor_b = float(entrada_b.replace(',','.'))
-        
         if(operacao == 'a' or operacao == 's'): # se for adição ou subtração, siga por aqui
-            if(operacao == 'a'): # se for adição, faça isso
+            if(operacao == 'a'): 
                 resultado = valor_a + valor_b
                 print(resultado)
-            else: # se for subtração, faça isso
+            else: 
                 resultado = valor_a - valor_b
                 print(resultado)
         else: # se não for nem adição, nem subtração então siga por aqui
-            if(operacao == 'm'): # se for multiplicação, faça iso
+            if(operacao == 'm'):
                 resultado = valor_a * valor_b
                 print(resultado)
-            else: # se for divisão, siga por aqui
-                if(valor_b != 0 and valor_a != 0): # se nenhum valor digitado for 0, faça isso
+            else:
+                if(valor_b != 0 and valor_a != 0): # Se nenhum valor digitado for 0, faça isso
                     resultado = valor_a / valor_b
                     print(resultado)
-                else: # se algum valor digitado for 0 numa divisão, faça isso
+                else: # Se algum valor digitado for 0 numa divisão, faça isso (ultima validação)
                     print('ERRO: Imagine que você tem 0 cookies e você divide eles igualmente entre 0 amigos. Quantos cookies cada pessoa ganha? Viu? Não faz sentido.')
                     print('O Cookie Monster está triste porque não há cookies, e você está triste porque não tem amigos.')
