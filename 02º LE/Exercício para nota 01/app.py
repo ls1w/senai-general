9d22e2ba9269ec99d023c32735fc0642f68ac98f
ls1w <marcel.alunos@cfp127aluno.local>
2023-08-28 14:20:33 -0300

    Exercício 1 completo

---
+++ 02º LE/Exercício para nota 01/app.py
@@ -0,0 +1,47 @@
+#***************************************************************************************************************************************
+# Objetivo: Receber dois valores inteiros e apresenta a diferença dos valores
+# Data: 23/08/2023
+# Autor: Lucas
+# Versão: 1.0
+#***************************************************************************************************************************************
+# Título
+print('')
+print('Calculando a diferença entre os valores')
+print('')
+erro = 0
+
+# entrada de dados
+while True:
+    try:
+        nota1 = int(input('Digite o primeiro número inteiro: '))
+    except ValueError:
+        print('')
+        print('ERRO: DIGITE SOMENTE VALORES VÁLIDOS!')
+        print('')
+        continue
+    else:
+        break
+
+while True:
+    try:
+        nota2 = int(input('Digite o segundo número inteiro: '))
+    except ValueError:
+        print('')
+        print('ERRO: DIGITE SOMENTE VALORES VÁLIDOS!')
+        print('')
+        continue
+    else:
+        break
+resultado = ''
+
+# processamento
+
+if(nota1 > nota2):
+    resultado = nota1 - nota2
+else:
+    resultado = nota2 - nota1
+
+# saída de dados
+print('')
+print('A diferença entre os valores', nota1,'e', nota2,' é de: ',resultado)
+print('')
