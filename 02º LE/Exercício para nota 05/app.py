1381bc98fe811d6d843b12eb513a1478c00c7b13
ls1w <marcel.alunos@cfp127aluno.local>
2023-08-29 14:12:43 -0300

    Exercício 05 app.py completo

---
+++ 02º LE/Exercício para nota 05/app.py
@@ -0,0 +1,124 @@
+#***************************************************************************************************************************************
+# Objetivo: Determinar se um aluno foi aprovado ou reprovado
+# Data: 23/08/2023
+# Autor: Lucas
+# Versão: 1.0
+#***************************************************************************************************************************************
+# Título
+print('')
+print('Situação do aluno')
+print('')
+
+
+# entrada de dados
+
+nome = input('Digite o nome do aluno: ')
+
+while True:
+    try:
+        nota1 = int(input('Digite a primeira nota do aluno: '))
+    except ValueError:
+        print('')
+        print('ERRO: DIGITE SOMENTE VALORES VÁLIDOS!')
+        print('')
+        continue
+    else:
+        if(nota1 < 0 or nota1 > 10):
+            print('')
+            print('ERRO: DIGITE SOMENTE VALORES ENTRE 0 E 10!')
+            print('')
+            continue
+        else:
+            break
+
+while True:
+    try:
+        nota2 = int(input('Digite a segunda nota do aluno: '))
+    except ValueError:
+        print('')
+        print('ERRO: DIGITE SOMENTE VALORES VÁLIDOS!')
+        print('')
+        continue
+    else:
+        if(nota2 < 0 or nota2 > 10):
+            print('')
+            print('ERRO: DIGITE SOMENTE VALORES ENTRE 0 E 10!')
+            print('')
+            continue
+        else:
+            break
+
+while True:
+    try:
+        nota3 = int(input('Digite a terceira nota do aluno: '))
+    except ValueError:
+        print('')
+        print('ERRO: DIGITE SOMENTE VALORES VÁLIDOS!')
+        print('')
+        continue
+    else:
+        if(nota3 < 0 or nota3 > 10):
+            print('')
+            print('ERRO: DIGITE SOMENTE VALORES ENTRE 0 E 10!')
+            print('')
+            continue
+        else:
+            break
+
+while True:
+    try:
+        nota4 = int(input('Digite a quarta nota do aluno: '))
+    except ValueError:
+        print('')
+        print('ERRO: DIGITE SOMENTE VALORES VÁLIDOS!')
+        print('')
+        continue
+    else:
+        if(nota4 < 0 or nota4 > 10):
+            print('')
+            print('ERRO: DIGITE SOMENTE VALORES ENTRE 0 E 10!')
+            print('')
+            continue
+        else:
+            break
+
+resultado = ''
+
+# processamento
+
+media = (nota1 + nota2 + nota3 + nota4)/4
+
+if(media >= 7):
+    print('')
+    print('Aluno',nome,'APROVADO com média de',media)
+    print('')
+elif(media < 4):
+    print('')
+    print('Aluno',nome,'REPROVADO com média de',media)
+    print('')
+else:
+    while True:
+        try:
+            nota_exame = int(input('Digite a nota do exame do aluno: '))
+        except ValueError:
+            print('')
+            print('ERRO: DIGITE SOMENTE VALORES VÁLIDOS!')
+            print('')
+            continue
+        else:
+            if(nota4 < 0 or nota4 > 10):
+                print('')
+                print('ERRO: DIGITE SOMENTE VALORES ENTRE 0 E 10!')
+                print('')
+                continue
+            else:
+                break
+    media_final = (media + nota_exame)/2
+    
+    if(media_final >= 5):
+        print('')
+        print('Aluno',nome,'APROVADO em exame com média final de',media_final)
+        print('')
+    else:
+        print('')
+        print('Aluno',nome,'REPROVAOD em exame com média final de',media_final)
