d2c250d9fbbef8feebe422f904c67d35300c65ba
ls1w <marcel.alunos@cfp127aluno.local>
2023-08-29 16:52:29 -0300

    Entrada exercício 09 parcialmente concluido

---
+++ 02º LE/Exercício para nota 09/app.py
@@ -0,0 +1,46 @@
+#***************************************************************************************************************************************
+# Objetivo: Programa para auxiliar na venda
+# Data: 23/08/2023
+# Autor: Lucas
+# Versão: 1.0
+#***************************************************************************************************************************************
+# Título
+print('')
+print('Maquininha de cartão')
+print('')
+
+# entrada de dados
+while True:
+    try:
+        valor_da_compra = input('Digite o primeiro valor: ')
+        valor_da_compra = float(valor_da_compra.replace(',','.'))
+    except ValueError:
+        print('')
+        print('ERRO: DIGITE SOMENTE VALORES NUMÉRICOS!')
+        print('')
+        continue
+    else:
+        if(valor_da_compra <= 0):
+            print('ERRO: DIGITE SOMENTE VALORES POSITIVOS')
+        else:
+            break
+
+while True:
+    try:
+        print('')
+        print('Codigo 1 = À vista, desconto de 8%')
+        print('Codigo 2 = À vista com cartão, desconto de 4%')
+        print('Codigo 3 = Em 2x sem juros')
+        print('Codigo 4 = Em 4x com juros de 8%')
+        print('')
+        codigo = int(input('Digite o codigo: '))
+    except ValueError:
+        print('')
+        print('ERRO: DIGITE SOMENTE VALORES VÁLIDOS!')
+        print('')
+        continue
+    else:
+        if(codigo < 1 or codigo > 4):
+            print('Opção inválida, escolha os codigos somente entre 1 e 4')
+        break
+
