212bd27142af473b4fd95be297f6c1a1a26efdbf
ls1w <marcel.alunos@cfp127aluno.local>
2023-08-31 16:49:20 -0300

    exercício 05 2.1

--- exercício 05/app.py
+++ exercício 05/app.py
@@ -2,14 +2,16 @@
 # Objetivo: Calcular uma tabuada
 # Data: 31/08/2023
 # Autor: Lucas
-# Versão: 1.0
+# Versão: 2.0
 # **********************************************************************************************************************************************
 # Título
 print('')
 print('Fazendo uma tabuada')
 
 # ENTRADA DE DADOS
+print('')
 tabuada = int(input('Escolha a tabuada desejada: '))
+print('')
 limiteMaximo = int(input('Escolha o limite máximo da tabuada: '))
 
 # PROCESSAMENTO
