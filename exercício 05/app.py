5926eae1dd580997b8cf32cfc6da34f2a1bc528e
ls1w <marcel.alunos@cfp127aluno.local>
2023-08-31 16:49:10 -0300

    exercício 05 2.0

--- exercício 05/app.py
+++ exercício 05/app.py
@@ -10,10 +10,11 @@ print('Fazendo uma tabuada')
 
 # ENTRADA DE DADOS
 tabuada = int(input('Escolha a tabuada desejada: '))
+limiteMaximo = int(input('Escolha o limite máximo da tabuada: '))
 
 # PROCESSAMENTO
 contador = 0
-while(contador <= 10):
+while(contador <= limiteMaximo):
     resultado = tabuada * contador
     print(tabuada, 'X', contador, '=', resultado)
     contador = contador + 1
