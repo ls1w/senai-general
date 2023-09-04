a3dec41d1c31406c06ba6f3371b4162529473768
ls1w <marcel.alunos@cfp127aluno.local>
2023-09-04 14:06:22 -0300

    Exercício 01 lista 03

---
+++ 03º LE/Exercício para nota 01/app.py
@@ -0,0 +1,17 @@
+#***************************************************************************************************************************************
+# Objetivo: Apresentar os quadrados dos números de 15 a 200
+# Data: 04/09/2023
+# Autor: Lucas
+# Versão: 1.0
+#***************************************************************************************************************************************
+# Título
+print('')
+print('Apresentando os quadrados')
+print('')
+
+contador = 15
+
+while(contador <= 200):
+    quadrado = contador ** 2
+    print(contador,'^ 2 =',quadrado)
+    contador = contador + 1
