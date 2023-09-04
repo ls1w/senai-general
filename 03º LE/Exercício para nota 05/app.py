ecef391916af42f0eb2e8362e512e3ba3674e9df
ls1w <marcel.alunos@cfp127aluno.local>
2023-09-04 16:55:46 -0300

    Exercício 05 lista 03 meio

---
+++ 03º LE/Exercício para nota 05/app.py
@@ -0,0 +1,73 @@
+#***************************************************************************************************************************************
+# Objetivo: Criar um sistema para gerenciar o cálculo de uma tabuada, conforme requisitos abaixo:  O sistema deverá solicitar as seguintes entradas:  •A tabuada inicial e tabuada final a ser calculada; •O número inicial e final do contador da tabuada;  Requisitos do Sistema  •A entrada da tabuada deverá ser entre 2 e 100, não sendo permitido outros valores;  •Nenhuma entrada de dados deverá ficar sem preenchimento;  •O valor até onde será calculada a tabuada deverá ser entre 1 e 50
+# Data: 04/09/2023
+# Autor: Lucas
+# Versão: 1.0
+#***************************************************************************************************************************************
+# Título
+print('')
+print('Calculando uma tabuada')
+print('')
+
+while True:
+    try:
+        tabuada_inicial = int(input('Digite o valor inicial da tabuada (entre 2 e 99): '))
+    except ValueError:
+        print('ERRO: Dgite somente valores numéricos inteiros')
+        continue
+    else:
+        if(tabuada_inicial < 2 or tabuada_inicial >= 100):
+            print('Os valores tem que ser entre 2 e 99')
+            continue
+        else:
+            break
+
+while True:
+    try:
+        tabuada_final = int(input('Digite o valor inicial da tabuada (entre 3 e 100): '))
+    except ValueError:
+        print('ERRO: Dgite somente valores numéricos inteiros')
+        continue
+    else:
+        if(tabuada_final <= 2 or tabuada_final > 100):
+            print('Os valores tem que ser entre 3 e 100')
+            continue
+        elif(tabuada_final <= tabuada_inicial):
+            print('O valor final da tabuada tem que ser maior que o valor inicial')
+            continue
+        else:
+            break
+
+while True:
+    try:
+        numero_inicial = int(input('Digite o valor inicial da tabuada (entre 1 e 49): '))
+    except ValueError:
+        print('ERRO: Dgite somente valores numéricos inteiros')
+        continue
+    else:
+        if(numero_inicial < 1 or numero_inicial >= 50):
+            print('Os valores tem que ser entre 1 e 49')
+            continue
+        else:
+            break
+
+while True:
+    try:
+        numero_final = int(input('Digite o valor inicial da tabuada (entre 2 e 50): '))
+    except ValueError:
+        print('ERRO: Dgite somente valores numéricos inteiros')
+        continue
+    else:
+        if(numero_final <= 1 or numero_final > 50):
+            print('Os valores tem que ser entre 2 e 50')
+            continue
+        elif(numero_final <= numero_inicial):
+            print('O valor final tem que ser maior que o valor inicial')
+            continue
+        else:
+            break
+
+contador = numero_inicial
+while(contador <= numero_final):
+    resultado = tabuada_inicial * contador
+    print(tabuada_inicial,'X',contador,'=',resultado)
