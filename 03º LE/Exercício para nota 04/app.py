6d4833dfbde30e22275f346ec5a9e1d113e89e0a
ls1w <marcel.alunos@cfp127aluno.local>
2023-09-05 16:31:06 -0300

    Exercício 04 Lista 03 ok

--- 03º LE/Exercício para nota 04/app.py
+++ 03º LE/Exercício para nota 04/app.py
@@ -2,27 +2,26 @@
 # Objetivo: Elaborar um programa que efetue a leitura de valores positivos inteiros até que um valor negativo seja informado. Ao final deverão ser apresentados o maior e o menor valores informados pelo usuário
 # Data: 04/09/2023
 # Autor: Lucas
-# Versão: 1.0
+# Versão: 1.5
 #***************************************************************************************************************************************
 # Título
 print('')
 print('Calculando os maiores valores até ser digitado um negativo')
 print('')
 
-
-while True:
+status = True
+while (status):
     try:
-        print('Nota: Numeros negativos param o programa')
-        contador = int(input('Digite um valor inteiro: '))
+        contador = int(input('Digite um valor inteiro positivo: '))
+        if(contador < 0):
+            print('')
+            print('ERRO: Valor inválido, digite somente valores positivos')
+        else:
+            status = False
     except ValueError:
+        print('')
         print('ERRO: Valor inválido, digite somente valores numéricos inteiros')
         continue
-    else:
-        if(contador < 0):
-            print('ERRO: Valor inválido, digite somente valores positivos')
-            continue
-        else:
-            break
 
 menor = contador
 maior = contador
@@ -32,10 +31,10 @@ maior = contador
 while(contador >= 0):
     if(contador >= maior):
         maior = contador
-        print('O último valor numérico digitado foi o MAIOR valor até então')
+        print('O último valor numérico inteiro digitado foi o MAIOR valor até então')
     elif(contador <= menor):
         menor = contador
-        print('O ultimo valor numérico digitado foi o MENOR valor até então')
+        print('O ultimo valor numérico inteiro digitado foi o MENOR valor até então')
     try:
         print('')
         print('Nota: Numeros negativos param o programa')
