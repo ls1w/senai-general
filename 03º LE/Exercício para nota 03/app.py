2b51b14f024c19ab21fb43f05aee29653f5a1dd7
ls1w <marcel.alunos@cfp127aluno.local>
2023-09-05 16:39:22 -0300

    Exercício 03 lista 03 improving

--- 03º LE/Exercício para nota 03/app.py
+++ 03º LE/Exercício para nota 03/app.py
@@ -10,16 +10,22 @@ print('Calculando fatoriais')
 print('')
 
 resultado_fatorial = 1
-try:
-    contador = int(input('Digite o número fatorial a ser calculado: '))
-except ValueError:
-    print('Digite somente números inteiros')
-else:
-    if(contador <= 0):
-        print('Digite somente valores positívos')
-    else:
-        while(contador > 0):
-            resultado_fatorial = contador * resultado_fatorial
-            print(contador,'X')
-            contador = contador - 1
-        print('=',resultado_fatorial)
+status = True
+while(status):
+    try:
+        contador = int(input('Digite o número fatorial a ser calculado: '))
+        if(contador <= 0):
+            print('')
+            print('ERRO: Digite somente valores positívos')
+        else:
+            status = False
+    except ValueError:
+        print('')
+        print('ERRO: Digite somente números inteiros')
+
+while(contador > 1):
+    resultado_fatorial = contador * resultado_fatorial
+    print(contador,'X')
+    contador = contador - 1
+print(1)
+print('=',resultado_fatorial)
