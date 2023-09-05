7328e8d035ca04eb2b3add8e20dab2f166ccbe2d
ls1w <marcel.alunos@cfp127aluno.local>
2023-09-05 16:29:43 -0300

    status = true

--- 03º LE/Exercício para nota 05/app.py
+++ 03º LE/Exercício para nota 05/app.py
@@ -9,53 +9,57 @@ print('')
 print('Calculando uma tabuada')
 print('')
 
-while True:
+status = True
+while(status):
     try:
         tabuada_inicial = int(input('Digite a tabuada inicial (entre 2 e 100): '))
         if(tabuada_inicial < 2 or tabuada_inicial > 100):
             print('')
             print('ERRO: Os valores tem que estar entre 2 e 100')
         else:
-            break
+            status = False
     except ValueError:
         print('')
         print('ERRO: Digite somente valores numéricos inteiros')
         continue
 
-while True:
+status = True
+while(status):
     try:
         tabuada_final = int(input('Digite a tabuada final (entre 2 e 100): '))
         if(tabuada_final < 2 or tabuada_final > 100 or tabuada_final < tabuada_inicial):
             print('')
             print('ERRO: Os valores tem que estar entre 2 e 100 e ser maior que',tabuada_inicial)
         else:
-            break
+            status = False
     except ValueError:
         print('')
         print('ERRO: Digite somente valores numéricos inteiros')
         continue
 
-while True:
+status = True
+while(status):
     try:
         numero_inicial = int(input('Digite o valor inicial na tabuada (entre 1 e 50): '))
         if(numero_inicial < 1 or numero_inicial > 50):
             print('')
             print('ERRO: Os valores tem que estar entre 1 e 50')
         else:
-            break
+            status = False
     except ValueError:
         print('')
         print('ERRO: Digite somente valores numéricos inteiros')
         continue
 
-while True:
+status = True
+while(status):
     try:
         numero_final = int(input('Digite o valor final na tabuada (entre 1 e 50): '))
         if(numero_final < 1 or numero_final > 50 or numero_final < numero_inicial):
             print('')
             print('ERRO: Os valores tem que estar entre 1 e 50 e ser maior que',numero_inicial)
         else:
-            break
+            status = False
     except ValueError:
         print('')
         print('ERRO: Digite somente valores numéricos inteiros')
